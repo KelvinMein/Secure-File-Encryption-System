a6e01135cd29a5519218b20fae389d6f8cae91e2
KelvinMein <meinkelvinto@gmail.com>
2026-06-15 18:15:46 +0300

    Delete Week-6-Symetric Ciphers II directory

--- Week-6-Symetric Ciphers II/avalance_test.py
+++
@@ -1,8 +0,0 @@
-a = 0b1010101010101010
-b = 0b1010101010101011
-
-difference = bin(a ^ b).count("1")
-
-print("Input A:", bin(a))
-print("Input B:", bin(b))
-print("Differing Bits:", difference)
--- Week-6-Symetric Ciphers II/permutation_demo.py
+++
@@ -1,4 +0,0 @@
-PBOX = [0,4,8,12,1,5,9,13,2,6,10,14,3,7,11,15]
-
-for i, p in enumerate(PBOX):
-    print(f"Bit {i} -> Position {p}")
