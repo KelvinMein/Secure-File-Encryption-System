8a39ea064f98c027a9395e80352c407e223551d9
KelvinMein <meinkelvinto@gmail.com>
2026-06-15 18:16:14 +0300

    Delete Week-7-Cryptoanalysis of Block Ciphers directory

--- Week-7-Cryptoanalysis of Block Ciphers/frequency_analysis.py
+++
@@ -1,9 +0,0 @@
-from collections import Counter
-
-ciphertext = "M@IIJ"
-
-freq = Counter(ciphertext)
-
-print("Character Frequencies:")
-for char, count in freq.items():
-    print(char, ":", count)
--- Week-7-Cryptoanalysis of Block Ciphers/xor_key_recovery.py
+++
@@ -1,6 +0,0 @@
-plaintext_char = 'H'
-ciphertext_char = 'M'
-
-key = ord(plaintext_char) ^ ord(ciphertext_char)
-
-print("Recovered Key:", key)
